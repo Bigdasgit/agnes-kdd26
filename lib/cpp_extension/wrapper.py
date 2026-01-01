f41785f22c27d5aef99206fb7c2c0bbda2018b6d
Bigdasgit <101625348+Bigdasgit@users.noreply.github.com>
2026-01-01 09:27:47 +0900

    Delete lib directory

--- lib/cpp_extension/wrapper.py
+++
@@ -1,7 +0,0 @@
-import os
-from torch.utils.cpp_extension import load
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-
-sample = load(name='sample', sources=[os.path.join(dir_path, 'block_based_sample.cpp')], extra_cflags=['-g', '-mavx512f', '-mavx512vl', '-mavx512bw','-Og', '-std=c++17', '-I'+ dir_path], extra_ldflags=['-lpthread','-ltbb', '-laio', '-lboost_system'])
-free = load(name='free', sources=[os.path.join(dir_path, 'free.cpp')], extra_cflags=['-O2'])
--- lib/utils.py
+++
@@ -1,5 +0,0 @@
-from lib.cpp_extension.wrapper import *
-
-
-def tensor_free(t):
-    free.tensor_free(t)
